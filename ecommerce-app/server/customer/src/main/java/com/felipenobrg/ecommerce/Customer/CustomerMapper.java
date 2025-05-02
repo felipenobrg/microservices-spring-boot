23eea0b901cba46e0a3a70d1722e978dda68fd99
felipenobrg <felipenobrega2012@gmail.com>
2025-05-02 14:18:31 -0300

    add customerMapper

---
+++ ecommerce-app/server/customer/src/main/java/com/felipenobrg/ecommerce/Customer/CustomerMapper.java
@@ -0,0 +1,20 @@
+package com.felipenobrg.ecommerce.Customer;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerMapper {
+
+    public Customer toCustomer(CustomerRequest request) {
+        if (request == null) {
+            return null;
+        }
+        return Customer.builder()
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .email(request.getEmail())
+                .address(request.getAddress())
+                .build();
+    }
+}
